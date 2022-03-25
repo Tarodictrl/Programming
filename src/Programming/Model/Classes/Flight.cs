2d45de27e9e57d1275903efde15b4e7605ec5cbd
nigga <vudi600@gmail.com>
2022-03-25 09:10:24 +0700

    Create class Flight

---
+++ src/Programming/Model/Classes/Flight.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming.Model.Classes
+{
+    public class Flight
+    {
+        private string _departurePoint;
+
+        private string _destination;
+
+        private int _flightTimeInMinutes;
+
+        public Flight()
+        {
+        }
+
+        public Flight(string departurePoint, string destination, int flightTimeInMinutes)
+        {
+            DeparturePoint = departurePoint;
+            Destination = destination;
+            FlightTimeInMinutes = flightTimeInMinutes;
+        }
+
+        public string DeparturePoint { get; set; }
+        public string Destination { get; set; }
+        public int FlightTimeInMinutes { get; set; }
+    }
+}
